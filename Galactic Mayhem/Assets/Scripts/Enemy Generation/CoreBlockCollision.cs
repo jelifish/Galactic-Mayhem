6b09e4d9ee03fbfb80d6cb3b279e5378264a8800
jelifish <jeffmunkee@gmail.com>
2015-10-18 23:40:04 -0700

    QOL update

--- Galactic Mayhem/Assets/Scripts/Enemy Generation/CoreBlockCollision.cs
+++ Galactic Mayhem/Assets/Scripts/Enemy Generation/CoreBlockCollision.cs
@@ -1,34 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ExteriorBlockCollision : MonoBehaviour {
-
-
-
-
+public class CoreBlockCollision : MonoBehaviour {
+	
 	public Material onState, offState;
 	float maxShield;
 	public float shield;
 	public float hull;
-
+	
 	void Start(){
 		maxShield = shield;
-
+		
 	}
-
-
-
-
-
-
-
+	
+	
+	
+	
+	
+	
+	
 	public float getShield(){
 		return shield;
 	}
 	public void takeDamage(float damage){
 		shield -= damage;
 		if (shield < 0) {
-		
+			
 			hull += shield;
 			shield = 0f;
 		}
@@ -41,66 +38,47 @@ public class ExteriorBlockCollision : MonoBehaviour {
 		shield += heal;
 		if (shield > maxShield) {
 			shield = maxShield;}
-
+		
 	}
 	private int selfInflicted;
-//	void OnCollisionEnter(Collision other)
-//	{
-//		if (other.gameObject.tag == "Enemy") {
-//			selfInflicted++;
-//				if(selfInflicted>5)
-//			{Destroy (this.gameObject);}
-//			return;
-//		
-//		}
-//			Destroy (other.gameObject);
-//			takeDamage (other.gameObject.GetComponent<Rigidbody> ().velocity.magnitude);
-//	}
+	//	void OnCollisionEnter(Collision other)
+	//	{
+	//		if (other.gameObject.tag == "Enemy") {
+	//			selfInflicted++;
+	//				if(selfInflicted>5)
+	//			{Destroy (this.gameObject);}
+	//			return;
+	//		
+	//		}
+	//			Destroy (other.gameObject);
+	//			takeDamage (other.gameObject.GetComponent<Rigidbody> ().velocity.magnitude);
+	//	}
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Enemy") {
-//			selfInflicted++;
-//			if(selfInflicted>5)
-//			{Destroy (this.gameObject);}
+			//			selfInflicted++;
+			//			if(selfInflicted>5)
+			//			{Destroy (this.gameObject);}
 			return;
 			
 		} else {
 			Destroy (other.gameObject);
-
 			takeDamage (other.gameObject.GetComponent<Rigidbody> ().velocity.magnitude);
 		}
 	}
 	public GameObject deathParticles;
 	
 	public void onDeath(){
+
+		foreach (Transform child in this.transform)
+		{
+			child.GetComponent<ExteriorBlockCollision>().onDeath();
+		}
+
 		Instantiate( deathParticles, this.transform.position, this.transform.rotation);
 	}
 	public void destroyObject()
 	{
 		Destroy(this.gameObject);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
---
+++ Galactic Mayhem/Assets/Scripts/Enemy Weapons/BlasterModule.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlasterModule : MonoBehaviour {
+	public Transform aim;
+	public Load load;
+	
+	public void shoot(Load load) 
+	{
+		Instantiate (load.projectile, aim.position + load.offset, aim.rotation * load.rotation);
+		//.	fireballSound.GetComponent<AudioSource> ().Play ();
+		//anim.SetBool (0, true);
+		//return load;
+	}
+
+}
